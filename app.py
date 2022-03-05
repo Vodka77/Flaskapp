01c390a6c8f15da9703041f7563424695c5b06e1
Vodka-X <95764863+Vodka77@users.noreply.github.com>
2022-03-05 14:02:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '03a789e8af223fc21a2896478b5cd84446c8eb595427b229'
+messages = [
+    
+            ]
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
